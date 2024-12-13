c0a574c553303599bf2c846b7d3fa4bfa830946d
Alex-maslo <4439897@gmail.com>
2024-12-13 15:57:33 +0200

    js-exam-v2

--- script.js
+++ script.js
@@ -1,11 +1,52 @@
-let obj1 = {
-  id: 1,
-  name: "vasya",
-  age: 33,
-  foo() {},
-  greeting() {},
-};
+const myArray = [];
 
-for (const key in obj1) {
-  console.log(typeof obj1[key]);
+function getInputValue(value) {
+  let array = value.split("=").map((el) => el.trim());
+
+  if (array.length === 2) {
+    return { name: array[0], value: array[1] };
+  } else {
+    alert(`Перевірте правильність введених значень`);
+    return 0;
+  }
 }
+
+function setData(array) {
+  let index = array.length;
+  return localStorage.setItem("myArray", JSON.stringify(array));
+}
+
+function getData() {
+  return JSON.parse(localStorage.getItem("myArray"));
+}
+
+function renderList() {
+  let list = document.getElementById("data-manager__list");
+  let data = getData();
+  list.innerHTML = "";
+  data.forEach(({ name, value }) => {
+    let listElement = document.createElement("li");
+    listElement.textContent = `${name} = ${value}`;
+    list.appendChild(listElement);
+  });
+}
+
+document.addEventListener("click", (event) => {
+  let inputData = document.getElementById("data-input");
+
+  switch (event.target.id) {
+    case "button-add":
+      myArray.push(getInputValue(inputData.value));
+      setData(myArray);
+      renderList();
+      break;
+
+    case "button-delete":
+      localStorage.clear();
+      break;
+  }
+});
+
+let vv = "            aaa=    hhh               ";
+
+console.log(getInputValue(vv));
